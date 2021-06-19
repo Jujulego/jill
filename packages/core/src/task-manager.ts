5b208c88f240c3bf9b6ff0223beeaddfb4de4c73
Julien Capellari <julien@capellari.net>
2021-06-20 00:03:02 +0200

    update(core): task manager start method

--- packages/core/src/task-manager.ts
+++ packages/core/src/task-manager.ts
@@ -1,3 +1,5 @@
+import os from 'os';
+
 import { Task } from './task';
 
 // Class
@@ -5,8 +7,19 @@ export class TaskManager {
   // Attributes
   private readonly _tasks: Task[] = [];
   private readonly _index = new Set<Task>();
+  private readonly _running = new Set<Task>();
+
+  // Constructor
+  constructor(
+    readonly jobs: number = os.cpus().length
+  ) {}
 
   // Methods
+  private _sortByComplexity() {
+    const cache = new Map<Task, number>();
+    this._tasks.sort((a, b) => a.complexity(cache) - b.complexity(cache));
+  }
+
   private _add(task: Task) {
     if (this._index.has(task)) return;
 
@@ -19,14 +32,34 @@ export class TaskManager {
     }
   }
 
-  private _sortByComplexity() {
-    const cache = new Map<Task, number>();
-    this._tasks.sort((a, b) => a.complexity(cache) - b.complexity(cache));
+  private _startNext(previous?: Task) {
+    if (previous) {
+      this._running.delete(previous);
+    }
+
+    for (const t of this._tasks) {
+      if (this._running.size >= this.jobs) {
+        break;
+      }
+
+      if (t.status === 'ready') {
+        this._running.add(t);
+
+        t.on('done', () => this._startNext(t));
+        t.on('failed', () => this._startNext(t));
+
+        t.start();
+      }
+    }
   }
 
   add(task: Task): void {
     this._add(task);
     this._sortByComplexity();
+  }
+
+  start(): void {
+    this._startNext();
   }
 
   // Properties
--- packages/core/tests/task-manager.test.ts
+++ packages/core/tests/task-manager.test.ts
@@ -1,10 +1,12 @@
 import { Task, TaskManager } from '../src';
+import { EventEmitter } from 'events';
+import cp from 'child_process';
 
 // Setup
 let manager: TaskManager;
 
 beforeEach(() => {
-  manager = new TaskManager();
+  manager = new TaskManager(1);
 });
 
 // Test suites
@@ -22,5 +24,48 @@ describe('TaskManager.add', () => {
   it('should add task and it\'s dependencies, then sort by complexity', () => {
     manager.add(ta);
     expect(manager.tasks).toEqual([tc, tb, ta]);
+  });
+});
+
+describe('TaskManager.start', () => {
+  // Setup
+  const ta = new Task('task-a');
+  const tb = new Task('task-b');
+  const tc = new Task('task-c');
+
+  ta.addDependency(tb); // complexity 3
+  ta.addDependency(tc); // complexity 1
+  tb.addDependency(tc); // complexity 0
+
+  // Tests
+  it('should start all tasks, one after another', () => {
+    jest.spyOn(ta, 'start');
+    jest.spyOn(tb, 'start');
+    jest.spyOn(tc, 'start');
+
+    const proc = new EventEmitter();
+
+    jest.spyOn(cp, 'spawn')
+      .mockReturnValue(proc as cp.ChildProcess);
+
+    // Start !
+    manager.add(ta);
+    manager.start();
+
+    // First task c should start
+    expect(ta.start).not.toBeCalled();
+    expect(tb.start).not.toBeCalled();
+    expect(tc.start).toBeCalled();
+
+    // When c completes b should start
+    proc.emit('close', 0);
+
+    expect(ta.start).not.toBeCalled();
+    expect(tb.start).toBeCalled();
+
+    // When b completes a should start
+    proc.emit('close', 0);
+
+    expect(ta.start).toBeCalled();
   });
 });
