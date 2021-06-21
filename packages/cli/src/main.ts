7ea226b948c5ad836de62b28cabc316f0863ce73
Julien Capellari <julien.capellari@gmail.com>
2021-06-21 19:14:11 +0200

    chore: use of jill in main workspace

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,7 +1,7 @@
 import { Project } from '@jujulego/jill-core';
 
 (async () => {
-  const project = new Project('../..');
+  const project = new Project('.');
 
   for await (const wks of project.workspaces()) {
     console.log(wks.name);
