a0d04055087a5b963e6a1254248f0ef0fde53363
Julien Capellari <julien.capellari@gmail.com>
2021-06-21 19:41:09 +0200

    add(jill-cli): add yargs

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,7 +1,21 @@
 import { Project } from '@jujulego/jill-core';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs';
 
+// Bootstrap
 (async () => {
-  const project = new Project('.');
+  // Options
+  const argv = await yargs(hideBin(process.argv))
+    .option('project', {
+      alias: 'p',
+      type: 'string',
+      description: 'Project root directory',
+      default: '.'
+    })
+    .argv;
+
+  // Run !
+  const project = new Project(argv.project);
 
   for await (const wks of project.workspaces()) {
     console.log(wks.name);
