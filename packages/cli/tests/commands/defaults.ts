4e0c1bf17d437ddaaf91e7d513f37d3870bb0739
Julien Capellari <julien@capellari.net>
2021-07-06 00:35:28 +0200

    fix(jill): unit tests

--- packages/cli/tests/commands/defaults.ts
+++ packages/cli/tests/commands/defaults.ts
@@ -1,7 +1,9 @@
+import { Arguments } from 'yargs';
+
 import { CommonArgs } from '../../src/wrapper';
 
 // Defaults
-export const defaultOptions: CommonArgs = {
+export const defaultOptions: Arguments<CommonArgs> = {
   project: '.',
   verbose: 0,
   '$0': 'jill',
