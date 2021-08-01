1827b3b47286f382df4d1543e0a55511a963e7f8
Julien Capellari <julien@capellari.net>
2021-08-01 23:46:33 +0200

    add(yarn-plugin): each command

--- packages/yarn/sources/index.ts
+++ packages/yarn/sources/index.ts
@@ -2,12 +2,14 @@ import type { Plugin } from '@yarnpkg/core';
 
 import { InfoCommand } from './commands/info';
 import { BuildCommand } from './commands/build';
+import { EachCommand } from './commands/each';
 
 // Plugin
 const plugin: Plugin = {
   commands: [
     InfoCommand,
     BuildCommand,
+    EachCommand
   ],
 };
 
