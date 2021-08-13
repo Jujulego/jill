d471a5a159e3fc7a6f11982009a4955700479736
Julien Capellari <julien.capellari@neoxia.com>
2021-08-13 18:47:46 +0200

    update(cli): e2e list tests

--- packages/cli/e2e/tests/info.test.ts
+++ packages/cli/e2e/tests/info.test.ts
@@ -1,5 +1,7 @@
-import { jill, MOCK } from '../utils';
 import chalk from 'chalk';
+import * as path from 'path';
+
+import { jill, MOCK } from '../utils';
 
 // Setup
 chalk.level = 1;
@@ -8,6 +10,16 @@ chalk.level = 1;
 describe('jill info', () => {
   it('should print main workspace data', async () => {
     await expect(jill(['info'], { cwd: MOCK })).
+      resolves.toMatchSnapshot();
+  });
+
+  it('should print given workspace data', async () => {
+    await expect(jill(['info', '-w', 'mock-test-a'], { cwd: MOCK })).
+      resolves.toMatchSnapshot();
+  });
+
+  it('should print current workspace data', async () => {
+    await expect(jill(['info'], { cwd: path.join(MOCK, 'workspaces/test-a') })).
       resolves.toMatchSnapshot();
   });
 });
