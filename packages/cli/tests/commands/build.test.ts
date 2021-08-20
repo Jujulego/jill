23090d4eb9ee00e988573c9cbbccb32bc9c2be40
Julien Capellari <julien.capellari@neoxia.com>
2021-08-20 17:55:52 +0200

    fix(cli): build command tests

--- packages/cli/tests/commands/build.test.ts
+++ packages/cli/tests/commands/build.test.ts
@@ -1,15 +1,12 @@
-import { EventListener, Project, TaskManager, TaskManagerEventMap, Workspace } from '@jujulego/jill-core';
-import chalk from 'chalk';
+import { Project, TaskManager, Workspace } from '@jujulego/jill-core';
 
-import { buildCommand, logger, OraLogger } from '../../src';
+import { buildCommand, logger } from '../../src';
 
 import { MockTask } from '../../mocks/task';
 import '../logger';
 
 // Setup
 jest.mock('../../src/logger');
-
-chalk.level = 1;
 
 let project: Project;
 
@@ -50,66 +47,31 @@ describe('jill build', () => {
     expect(logger.fail).toHaveBeenCalledWith('Workspace . not found');
   });
 
-  it('should print tasks status', async () => {
+  it('should exit 0 when manager is finished', async () => {
     const wks = new Workspace('./wks', { name: 'wks', version: '1.0.0' }, project);
-    const tsk1 = new MockTask('test', ['1'], { context: { workspace: wks }});
-    const tsk2 = new MockTask('test', ['2'], { context: { workspace: wks }});
-    const tsk3 = new MockTask('test', ['3'], { context: { workspace: wks }});
-    const handlers: { [K in keyof TaskManagerEventMap]?: EventListener<TaskManagerEventMap, K> } = {};
+    const tsk = new MockTask('test', [], { context: { workspace: wks }});
 
     jest.spyOn(project, 'workspace').mockResolvedValue(wks);
-    jest.spyOn(wks, 'build').mockResolvedValue(tsk1);
+    jest.spyOn(wks, 'build').mockResolvedValue(tsk);
 
     jest.spyOn(TaskManager.prototype, 'add').mockImplementation();
+    jest.spyOn(TaskManager.prototype, 'on').mockImplementation();
     jest.spyOn(TaskManager.prototype, 'start').mockImplementation();
-    jest.spyOn(TaskManager.prototype, 'on')
-      .mockImplementation(function (event, handler) {
-        handlers[event] = handler;
-        return this;
-      });
+    jest.spyOn(TaskManager.prototype, 'waitFor').mockResolvedValue([]);
 
     // Call
     await expect(buildCommand(project, { workspace: 'wks' }))
-      .resolves.toBeUndefined();
+      .resolves.toBe(0);
 
     // Checks
     expect(logger.spin).toHaveBeenCalledWith('Loading project');
     expect(project.workspace).toHaveBeenCalledWith('wks');
     expect(wks.build).toHaveBeenCalled();
-    expect(TaskManager.prototype.add).toHaveBeenCalledWith(tsk1);
+    expect(TaskManager.prototype.add).toHaveBeenCalledWith(tsk);
     expect(TaskManager.prototype.start).toHaveBeenCalled();
     expect(TaskManager.prototype.on).toHaveBeenCalledWith('started', expect.any(Function));
     expect(TaskManager.prototype.on).toHaveBeenCalledWith('completed', expect.any(Function));
-
-    // Activate task 1
-    (logger.spin as jest.MockedFunction<typeof OraLogger.prototype.spin>).mockClear();
-
-    handlers.started!(tsk1);
-    expect(logger.spin).toHaveBeenCalledWith('Building wks ...');
-
-    // Activate task 2 & 3
-    (logger.spin as jest.MockedFunction<typeof OraLogger.prototype.spin>).mockClear();
-
-    handlers.started!(tsk2);
-    handlers.started!(tsk3);
-    expect(logger.spin).toHaveBeenCalledWith('Building 2 packages ...');
-    expect(logger.spin).toHaveBeenCalledWith('Building 3 packages ...');
-
-    // Complete task 3
-    (logger.spin as jest.MockedFunction<typeof OraLogger.prototype.spin>).mockClear();
-    (logger.succeed as jest.MockedFunction<typeof OraLogger.prototype.succeed>).mockClear();
-    (logger.fail as jest.MockedFunction<typeof OraLogger.prototype.succeed>).mockClear();
-
-    handlers.completed!(tsk2.setStatus('failed'));
-    expect(logger.fail).toHaveBeenCalledWith('Failed to build wks');
-    expect(logger.spin).toHaveBeenCalledWith('Building 2 packages ...');
-
-    // Complete task 1 & 2
-    (logger.succeed as jest.MockedFunction<typeof OraLogger.prototype.succeed>).mockClear();
-
-    handlers.completed!(tsk2.setStatus('done'));
-    handlers.completed!(tsk1.setStatus('done'));
-    expect(logger.succeed).toHaveBeenCalledWith('wks built');
+    expect(TaskManager.prototype.waitFor).toHaveBeenCalledWith('finished');
   });
 
   it('should use current workspace', async () => {
@@ -121,19 +83,22 @@ describe('jill build', () => {
     jest.spyOn(wks, 'build').mockResolvedValue(tsk);
 
     jest.spyOn(TaskManager.prototype, 'add').mockImplementation();
-    jest.spyOn(TaskManager.prototype, 'start').mockImplementation();
     jest.spyOn(TaskManager.prototype, 'on').mockReturnThis();
+    jest.spyOn(TaskManager.prototype, 'start').mockImplementation();
+    jest.spyOn(TaskManager.prototype, 'waitFor').mockResolvedValue([]);
 
     // Call
     await expect(buildCommand(project, { workspace: undefined }))
-      .resolves.toBeUndefined();
+      .resolves.toBe(0);
 
     // Checks
     expect(logger.spin).toHaveBeenCalledWith('Loading project');
-    expect(project.workspace).not.toHaveBeenCalled();
     expect(project.currentWorkspace).toHaveBeenCalled();
     expect(wks.build).toHaveBeenCalled();
     expect(TaskManager.prototype.add).toHaveBeenCalledWith(tsk);
     expect(TaskManager.prototype.start).toHaveBeenCalled();
+    expect(TaskManager.prototype.on).toHaveBeenCalledWith('started', expect.any(Function));
+    expect(TaskManager.prototype.on).toHaveBeenCalledWith('completed', expect.any(Function));
+    expect(TaskManager.prototype.waitFor).toHaveBeenCalledWith('finished');
   });
 });
