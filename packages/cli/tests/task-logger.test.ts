37c8ec168514568ed5764a5b0eab98d6162bad63
Julien Capellari <julien@capellari.net>
2021-08-21 00:26:50 +0200

    update(cli): test task logger

---
+++ packages/cli/tests/task-logger.test.ts
@@ -0,0 +1,136 @@
+import { Task, TaskManager, Workspace, Project } from '@jujulego/jill-core';
+
+import { TaskLogger } from '../src/task-logger';
+import { logger } from '../src';
+
+import './logger';
+
+// Constants
+const prj = new Project('prj');
+const wks1 = new Workspace('wks-1', { name: 'wks-1' }, prj);
+
+// Setup
+let mng: TaskManager;
+let tsk1: Task;
+let tsk2: Task;
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+
+  // Mocks
+  mng = new TaskManager();
+
+  tsk1 = new Task('task', [], { cwd: 'prj/wks-1', context: { workspace: wks1 } });
+  tsk2 = new Task('task', [], { cwd: 'prj/wks-2' });
+
+  mng.add(tsk1);
+  mng.add(tsk2);
+
+  jest.spyOn(logger, 'spin').mockImplementation();
+  jest.spyOn(logger, 'succeed').mockImplementation();
+  jest.spyOn(logger, 'fail').mockImplementation();
+});
+
+// Tests
+it('should call given format functions', () => {
+  const log = new TaskLogger();
+  log.connect(mng);
+
+  // Spy formats
+  const spySpinMultiple = jest.fn(() => "");
+  log.on('spin-multiple', spySpinMultiple);
+
+  const spySpinSimple = jest.fn(() => "");
+  log.on('spin-simple', spySpinSimple);
+
+  const spySucceed = jest.fn(() => "");
+  log.on('succeed', spySucceed);
+
+  const spyFail = jest.fn(() => "");
+  log.on('fail', spyFail);
+
+  // 1 task started
+  jest.spyOn(tsk1, 'status', 'get').mockReturnValue('running');
+  mng.emit('started', tsk1);
+
+  expect(spySpinSimple).toHaveBeenCalledWith(tsk1);
+
+  // 2 task started
+  jest.spyOn(tsk2, 'status', 'get').mockReturnValue('running');
+  mng.emit('started', tsk2);
+
+  expect(spySpinMultiple).toHaveBeenCalledWith(2);
+
+  // succeed 1st task
+  jest.spyOn(tsk1, 'status', 'get').mockReturnValue('done');
+  mng.emit('completed', tsk1);
+
+  expect(spySpinSimple).toHaveBeenCalledWith(tsk2);
+  expect(spySucceed).toHaveBeenCalledWith(tsk1);
+
+  // fail 2nd task
+  jest.spyOn(tsk2, 'status', 'get').mockReturnValue('failed');
+  mng.emit('completed', tsk2);
+
+  expect(spyFail).toHaveBeenCalledWith(tsk2);
+});
+
+it('should print default messages', () => {
+  const log = new TaskLogger();
+  log.connect(mng);
+
+  // 1 task started
+  jest.spyOn(tsk1, 'status', 'get').mockReturnValue('running');
+  mng.emit('started', tsk1);
+
+  expect(logger.spin).toHaveBeenCalledWith('Building wks-1 ...');
+
+  // 2 task started
+  jest.spyOn(tsk2, 'status', 'get').mockReturnValue('running');
+  mng.emit('started', tsk2);
+
+  expect(logger.spin).toHaveBeenCalledWith('Building 2 workspaces ...');
+
+  // succeed 1st task
+  jest.spyOn(tsk1, 'status', 'get').mockReturnValue('done');
+  mng.emit('completed', tsk1);
+
+  expect(logger.succeed).toHaveBeenCalledWith('wks-1 built');
+  expect(logger.spin).toHaveBeenCalledWith('Building prj/wks-2 ...');
+
+  // fail 2nd task
+  jest.spyOn(tsk2, 'status', 'get').mockReturnValue('failed');
+  mng.emit('completed', tsk2);
+
+  expect(logger.fail).toHaveBeenCalledWith('Failed to build prj/wks-2');
+});
+
+it('should print default messages (2)', () => {
+  const log = new TaskLogger();
+  log.connect(mng);
+
+  // 1 task started
+  jest.spyOn(tsk1, 'status', 'get').mockReturnValue('running');
+  mng.emit('started', tsk1);
+
+  expect(logger.spin).toHaveBeenCalledWith('Building wks-1 ...');
+
+  // 2 task started
+  jest.spyOn(tsk2, 'status', 'get').mockReturnValue('running');
+  mng.emit('started', tsk2);
+
+  expect(logger.spin).toHaveBeenCalledWith('Building 2 workspaces ...');
+
+  // fail 1st task
+  jest.spyOn(tsk1, 'status', 'get').mockReturnValue('failed');
+  mng.emit('completed', tsk1);
+
+  expect(logger.fail).toHaveBeenCalledWith('Failed to build wks-1');
+  expect(logger.spin).toHaveBeenCalledWith('Building prj/wks-2 ...');
+
+  // succeed 2nd task
+  jest.spyOn(tsk2, 'status', 'get').mockReturnValue('done');
+  mng.emit('completed', tsk2);
+
+  expect(logger.succeed).toHaveBeenCalledWith('prj/wks-2 built');
+});
