7d6bf3128fb5da46b645f9fc1c9cee9161583369
Julien Capellari <julien.capellari@neoxia.com>
2021-08-27 10:24:36 +0200

    fix(core): missing manifest declaration

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 export * from './event-emitter';
 export * from './logger';
-export type { Manifest } from './manifest';
+export * from './manifest';
 export * from './project';
 export * from './task';
 export * from './task-manager';
