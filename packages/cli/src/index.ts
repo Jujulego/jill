db9e5a3a75b671a818c39469730f6f77811bc9b9
Julien Capellari <julien@capellari.net>
2021-09-09 00:05:32 +0200

    fix(cli): build errors

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,11 +1,9 @@
-export { buildCommand } from './commands/build';
 export { eachCommand } from './commands/each';
 export { infoCommand } from './commands/info';
 export { listCommand } from './commands/list';
 export { runCommand } from './commands/run';
 export { logger, OraLogger } from './logger';
 
-export type { BuildArgs } from './commands/build';
 export type { EachArgs } from './commands/each';
 export type { InfoArgs } from './commands/info';
 export type { RunArgs } from './commands/run';
