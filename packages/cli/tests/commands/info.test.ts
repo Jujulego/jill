7186d29ea4a2eff80d22869f1443386cd674651a
Julien Capellari <julien.capellari@gmail.com>
2021-12-12 14:54:18 +0100

    feat(myr): adapt info tests

--- packages/cli/tests/commands/info.test.ts
+++ packages/cli/tests/commands/info.test.ts
@@ -1,7 +1,9 @@
 import { Project, Workspace } from '@jujulego/jill-core';
+import { ExitError } from '@oclif/core/lib/errors';
 import chalk from 'chalk';
 
-import { infoCommand, logger } from '../../src';
+import InfoCommand from '../../src/_commands/info';
+import { logger } from '../../src';
 
 // Setup
 jest.mock('../../src/logger');
@@ -16,9 +18,12 @@ beforeEach(() => {
   screen = '';
 
   // Mocks
+  jest.resetAllMocks();
   jest.restoreAllMocks();
 
+  jest.spyOn(InfoCommand.prototype, 'project', 'get').mockReturnValue(project);
   jest.spyOn(console, 'log').mockImplementation((message) => screen += message + '\n');
+  jest.spyOn(process, 'emitWarning').mockImplementation((err: string | Error) => process.stdout.write((typeof err === 'string' ? err : err.message) + '\n'));
 });
 
 // Tests
@@ -27,13 +32,13 @@ describe('jill info', () => {
     jest.spyOn(project, 'workspace').mockResolvedValue(null);
 
     // Call
-    await expect(infoCommand(project, { workspace: 'does-not-exists' }))
-      .resolves.toBe(1);
+    await expect(InfoCommand.run(['-w', 'does-not-exists']))
+      .rejects.toEqual(new ExitError(1));
 
     // Checks
     expect(logger.spin).toHaveBeenCalledWith('Loading project');
     expect(project.workspace).toHaveBeenCalledWith('does-not-exists');
-    expect(logger.fail).toHaveBeenCalledWith('Workspace does-not-exists not found');
+    expect(logger.fail).toHaveBeenCalledWith('Workspace \'does-not-exists\' not found');
   });
 
   it('should exit 1 if current workspace not found', async () => {
@@ -41,24 +46,24 @@ describe('jill info', () => {
     jest.spyOn(project, 'currentWorkspace').mockResolvedValue(null);
 
     // Call
-    await expect(infoCommand(project, { workspace: undefined }))
-      .resolves.toBe(1);
+    await expect(InfoCommand.run([]))
+      .rejects.toEqual(new ExitError(1));
 
     // Checks
     expect(logger.spin).toHaveBeenCalledWith('Loading project');
     expect(project.workspace).not.toHaveBeenCalled();
     expect(project.currentWorkspace).toHaveBeenCalled();
-    expect(logger.fail).toHaveBeenCalledWith('Workspace . not found');
+    expect(logger.fail).toHaveBeenCalledWith('Workspace \'.\' not found');
   });
 
   it('should print workspace basic info', async () => {
-    const wks = new Workspace('./wks', { name: 'wks', version: '1.0.0' }, project);
+    const wks = new Workspace('./wks', { name: 'wks', version: '1.0.0' } as any, project);
 
     jest.spyOn(project, 'workspace').mockResolvedValue(wks);
 
     // Call
-    await expect(infoCommand(project, { workspace: 'wks' }))
-      .resolves.toBe(0);
+    await expect(InfoCommand.run(['-w', 'wks']))
+      .resolves.toBeUndefined();
 
     // Checks
     expect(logger.spin).toHaveBeenCalledWith('Loading project');
@@ -68,14 +73,14 @@ describe('jill info', () => {
   });
 
   it('should use current workspace', async () => {
-    const wks = new Workspace('./wks', { name: 'wks', version: '1.0.0' }, project);
+    const wks = new Workspace('./wks', { name: 'wks', version: '1.0.0' } as any, project);
 
     jest.spyOn(project, 'workspace');
     jest.spyOn(project, 'currentWorkspace').mockResolvedValue(wks);
 
     // Call
-    await expect(infoCommand(project, { workspace: undefined }))
-      .resolves.toBe(0);
+    await expect(InfoCommand.run([]))
+      .resolves.toBeUndefined();
 
     // Checks
     expect(logger.spin).toHaveBeenCalledWith('Loading project');
@@ -85,11 +90,11 @@ describe('jill info', () => {
 
   it('should print workspace basic info with dependencies', async () => {
     jest.spyOn(project, 'workspace')
-      .mockImplementation(async (name = 'wks') => new Workspace('./wks', { name, version: '1.0.0', dependencies: { depA: '1.0.0', depB: '1.0.0' } }, project));
+      .mockImplementation(async (name = 'wks') => new Workspace('./wks', { name, version: '1.0.0', dependencies: { depA: '1.0.0', depB: '1.0.0' } } as any, project));
 
     // Call
-    await expect(infoCommand(project, { workspace: 'wks' }))
-      .resolves.toBe(0);
+    await expect(InfoCommand.run(['-w', 'wks']))
+      .resolves.toBeUndefined();
 
     // Checks
     expect(logger.spin).toHaveBeenCalledWith('Loading project');
@@ -100,11 +105,11 @@ describe('jill info', () => {
 
   it('should print workspace basic info with dev-dependencies', async () => {
     jest.spyOn(project, 'workspace')
-      .mockImplementation(async (name = 'wks') => new Workspace('./wks', { name, version: '1.0.0', devDependencies: { depA: '1.0.0', depB: '1.0.0' } }, project));
+      .mockImplementation(async (name = 'wks') => new Workspace('./wks', { name, version: '1.0.0', devDependencies: { depA: '1.0.0', depB: '1.0.0' } } as any, project));
 
     // Call
-    await expect(infoCommand(project, { workspace: 'wks' }))
-      .resolves.toBe(0);
+    await expect(InfoCommand.run(['-w', 'wks']))
+      .resolves.toBeUndefined();
 
     // Checks
     expect(logger.spin).toHaveBeenCalledWith('Loading project');
