79fca3418ee4b8e1023675f18cd4d7a3e4f01323
Julien Capellari <julien@capellari.net>
2021-12-31 18:07:15 +0100

    feat(myr): define myr plugin

---
+++ packages/myr/src/index.ts
@@ -0,0 +1,12 @@
+import { Plugin } from '@jujulego/jill-common';
+
+import { MyrCommand } from './myr.command';
+import { WatchCommand } from './watch.command';
+
+// Plugin
+const myrPlugin = Plugin.createPlugin('myr', [
+  MyrCommand,
+  WatchCommand
+]);
+
+export default myrPlugin;
