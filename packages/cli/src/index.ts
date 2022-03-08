96fd966831c3ef68870879eada5baea7aeb42174
Julien Capellari <julien.capellari@neoxia.com>
2022-03-08 19:18:15 +0100

    fix(cli): build

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,4 @@
 export { EachCommand } from './commands/each.command';
-export { InfoCommand } from './commands/info.command';
+export { TreeCommand } from './commands/tree.command';
 export { ListCommand } from './commands/list.command';
 export { RunCommand } from './commands/run.command';
