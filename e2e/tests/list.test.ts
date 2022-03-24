d52cd37856b352202068cd7dec48a690cbba4b34
Julien Capellari <julien.capellari@neoxia.com>
2022-03-24 16:39:55 +0100

    fix(e2e): list tests

--- e2e/tests/list.test.ts
+++ e2e/tests/list.test.ts
@@ -6,127 +6,120 @@ import { jill, MOCK } from '../utils';
 // Tests suites
 describe('jill list', () => {
   it('should print list of all projects', async () => {
-    await expect(jill(['list'], { cwd: MOCK }))
-      .resolves.toEqual({
-        code: 0,
-        stderr: [
-          '- Loading project'
-        ],
-        stdout: [
-          chalk`mock-root  `,
-          chalk`mock-test-a`,
-          chalk`mock-test-b`,
-          chalk`mock-test-c`,
-          chalk`mock-test-d`,
-        ]
-      });
+    const res = await jill(['list'], { cwd: MOCK });
+
+    expect(res.code).toBe(0);
+    expect(res.frames).toEqual([
+      ['⠋ Loading project ...'],
+      ['⠋ Load workspaces'],
+      [
+        'mock-root',
+        'mock-test-a',
+        'mock-test-b',
+        'mock-test-c',
+        'mock-test-d'
+      ]
+    ]);
   });
 
   it('should print list of all private projects (--private)', async () => {
-    await expect(jill(['list', '--private'], { cwd: MOCK }))
-      .resolves.toEqual({
-        code: 0,
-        stderr: [
-          '- Loading project'
-        ],
-        stdout: [
-          chalk`mock-root`,
-        ]
-      });
+    const res = await jill(['list', '--private'], { cwd: MOCK });
+
+    expect(res.code).toBe(0);
+    expect(res.frames).toEqual([
+      ['⠋ Loading project ...'],
+      ['⠋ Load workspaces'],
+      ['mock-root']
+    ]);
   });
 
   it('should print list of all public projects (--no-private)', async () => {
-    await expect(jill(['list', '--no-private'], { cwd: MOCK }))
-      .resolves.toEqual({
-        code: 0,
-        stderr: [
-          '- Loading project'
-        ],
-        stdout: [
-          chalk`mock-test-a`,
-          chalk`mock-test-b`,
-          chalk`mock-test-c`,
-          chalk`mock-test-d`,
-        ]
-      });
+    const res = await jill(['list', '--no-private'], { cwd: MOCK });
+
+    expect(res.code).toBe(0);
+    expect(res.frames).toEqual([
+      ['⠋ Loading project ...'],
+      ['⠋ Load workspaces'],
+      [
+        'mock-test-a',
+        'mock-test-b',
+        'mock-test-c',
+        'mock-test-d'
+      ]
+    ]);
   });
 
   it('should print list of all public projects (--with-script start)', async () => {
-    await expect(jill(['list', '--with-script', 'start'], { cwd: MOCK }))
-      .resolves.toEqual({
-        code: 0,
-        stderr: [
-          '- Loading project'
-        ],
-        stdout: [
-          chalk`mock-test-a`,
-        ]
-      });
+    const res = await jill(['list', '--with-script', 'start'], { cwd: MOCK });
+
+    expect(res.code).toBe(0);
+    expect(res.frames).toEqual([
+      ['⠋ Loading project ...'],
+      ['⠋ Load workspaces'],
+      ['mock-test-a']
+    ]);
   });
 
   it('should print name and version of all projects (--attrs name version)', async () => {
-    await expect(jill(['list', '--attrs', 'name', 'version'], { cwd: MOCK }))
-      .resolves.toEqual({
-        code: 0,
-        stderr: [
-          '- Loading project'
-        ],
-        stdout: [
-          chalk`{bold Name       }  {bold Version}`,
-          chalk`mock-root    {grey unset}  `,
-          chalk`mock-test-a  {grey unset}  `,
-          chalk`mock-test-b  {grey unset}  `,
-          chalk`mock-test-c  {grey unset}  `,
-          chalk`mock-test-d  {grey unset}  `,
-        ]
-      });
+    const res = await jill(['list', '--attrs', 'name', 'version'], { cwd: MOCK });
+
+    expect(res.code).toBe(0);
+    expect(res.frames).toEqual([
+      ['⠋ Loading project ...'],
+      ['⠋ Load workspaces'],
+      [
+        chalk`{bold Name}         {bold Version}`,
+        chalk`mock-root    {grey unset}`,
+        chalk`mock-test-a  {grey unset}`,
+        chalk`mock-test-b  {grey unset}`,
+        chalk`mock-test-c  {grey unset}`,
+        chalk`mock-test-d  {grey unset}`,
+      ]
+    ]);
   });
 
   it('should print list of all projects with headers (--headers)', async () => {
-    await expect(jill(['list', '--headers'], { cwd: MOCK }))
-      .resolves.toEqual({
-        code: 0,
-        stderr: [
-          '- Loading project'
-        ],
-        stdout: [
-          chalk`{bold Name       }`,
-          chalk`mock-root  `,
-          chalk`mock-test-a`,
-          chalk`mock-test-b`,
-          chalk`mock-test-c`,
-          chalk`mock-test-d`,
-        ]
-      });
+    const res = await jill(['list', '--headers'], { cwd: MOCK });
+
+    expect(res.code).toBe(0);
+    expect(res.frames).toEqual([
+      ['⠋ Loading project ...'],
+      ['⠋ Load workspaces'],
+      [
+        chalk`{bold Name}`,
+        chalk`mock-root`,
+        chalk`mock-test-a`,
+        chalk`mock-test-b`,
+        chalk`mock-test-c`,
+        chalk`mock-test-d`,
+      ]
+    ]);
   });
 
   it('should print list of all projects in long format (-l)', async () => {
-    await expect(jill(['list', '-l'], { cwd: MOCK }))
-      .resolves.toEqual({
-        code: 0,
-        stderr: [
-          '- Loading project'
-        ],
-        stdout: [
-          chalk`{bold Name       }  {bold Version}  {bold Root             }`,
-          chalk`mock-root    {grey unset}    .                `,
-          chalk`mock-test-a  {grey unset}    workspaces${path.sep}test-a`,
-          chalk`mock-test-b  {grey unset}    workspaces${path.sep}test-b`,
-          chalk`mock-test-c  {grey unset}    workspaces${path.sep}test-c`,
-          chalk`mock-test-d  {grey unset}    workspaces${path.sep}test-d`,
-        ]
-      });
+    const res = await jill(['list', '-l'], { cwd: MOCK });
+
+    expect(res.code).toBe(0);
+    expect(res.frames).toEqual([
+      ['⠋ Loading project ...'],
+      ['⠋ Load workspaces'],
+      [
+        chalk`{bold Name}         {bold Version}  {bold Root}`,
+        chalk`mock-root    {grey unset}    .`,
+        chalk`mock-test-a  {grey unset}    workspaces${path.sep}test-a`,
+        chalk`mock-test-b  {grey unset}    workspaces${path.sep}test-b`,
+        chalk`mock-test-c  {grey unset}    workspaces${path.sep}test-c`,
+        chalk`mock-test-d  {grey unset}    workspaces${path.sep}test-d`,
+      ]
+    ]);
   });
 
   it('should print list of all projects in json format (--json)', async () => {
-    const result = await jill(['list', '--json'], { cwd: MOCK });
+    const res = await jill(['list', '--json'], { cwd: MOCK });
 
-    expect(result.code).toBe(0);
-    expect(result.stderr).toEqual([
-      '- Loading project'
-    ]);
-
-    expect(JSON.parse(result.stdout.join('\n'))).toEqual([
+    expect(res.code).toBe(0);
+    console.log(res.lastFrame);
+    expect(JSON.parse(res.lastFrame.join('\n'))).toEqual([
       {
         name: 'mock-root',
         version: undefined,
--- e2e/utils.ts
+++ e2e/utils.ts
@@ -6,9 +6,12 @@ export const MAIN = path.join(__filename, '../../packages/cli/bin/jill.js');
 export const MOCK = path.join(__filename, '../../mock');
 export const ROOT = path.join(__filename, '../../');
 
+const INK_NEW_FRAME = '\u001b[2K\u001b[1A\u001b[2K\u001b[G';
+
 // Type
 export interface SpawnResult {
-  stdout: string[];
+  lastFrame: string[];
+  frames: string[][];
   stderr: string[];
   code: number;
 }
@@ -30,17 +33,35 @@ export function jill(args: ReadonlyArray<string>, opts: SpawnOptions = {}): Prom
 
     // Gather result
     const res: SpawnResult = {
-      stdout: [],
+      lastFrame: [],
+      frames: [[]],
       stderr: [],
       code: 0
     };
 
+    res.lastFrame = res.frames[0];
+
     proc.stdout.on('data', (msg: Buffer) => {
-      res.stdout.push(...msg.toString('utf-8').replace(/\n$/, '').split('\n'));
+      const data = msg.toString('utf-8');
+      console.log(JSON.stringify(data));
+      const frames = data.split(INK_NEW_FRAME);
+
+      // First one complete previous
+      if (frames[0] !== '') {
+        res.lastFrame.push(...frames[0].replace(/\n$/, '').split('\n'));
+      }
+
+      // New frames
+      for (const frame of frames.slice(1)) {
+        res.frames.push(frame.replace(/\n$/, '').split('\n'));
+      }
+
+      res.lastFrame = res.frames[res.frames.length - 1];
     });
 
     proc.stderr.on('data', (msg: Buffer) => {
-      res.stderr.push(...msg.toString('utf-8').replace(/\n$/, '').split('\n'));
+      const data = msg.toString('utf-8').replace(/\r?\n$/, '');
+      res.stderr.push(...data.split(/\r?\n/));
     });
 
     // Emit result
