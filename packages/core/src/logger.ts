28fa9c3208f7c7fab3f8cb80639801e3654f0ccf
Julien Capellari <julien.capellari@neoxia.com>
2022-10-11 19:20:24 +0200

    feat(core): setup logger transport

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,8 +1,19 @@
-import winston, { format } from 'winston';
+import chalk from 'chalk';
+import winston from 'winston';
 
 // Setup root logger
 export const logger = winston.createLogger({
-  format: format.combine(
-    format.timestamp(),
-  )
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.ms(),
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.errors(),
+        winston.format.colorize({ message: true, colors: { debug: 'grey', verbose: 'blue', info: 'white', error: 'red' } }),
+        winston.format.printf(({ label, message }) => message.split('\n').map(line => [label && chalk.grey(`[${label}]`), line].filter(p => p).join(' ')).join('\n')),
+      )
+    })
+  ]
 });
