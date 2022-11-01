27201c3fa918d03409cd31b8bc27396066f1e9c0
Julien Capellari <julien.capellari@gmail.com>
2022-11-01 23:46:36 +0100

    feat: test <TaskSpinner>

---
+++ tests/ui/task-spinner.test.tsx
@@ -0,0 +1,57 @@
+import { SpawnTask } from '@jujulego/tasks';
+import { render, cleanup } from 'ink-testing-library';
+
+import { TaskSpinner } from '../../src/ui';
+
+// Setup
+let task: SpawnTask;
+
+beforeEach(() => {
+  task = new SpawnTask('cmd', [], {});
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+// Tests
+describe('<TaskSpinner>', () => {
+  it('should print task name preceded by a spinner', () => {
+    const { lastFrame } = render(<TaskSpinner task={task} />);
+
+    // eslint-disable-next-line quotes
+    expect(lastFrame()).toMatchInlineSnapshot(`"⠂ cmd"`);
+  });
+
+  it('should print task name with running spinner', () => {
+    const { lastFrame } = render(<TaskSpinner task={task} />);
+
+    jest.spyOn(task, 'status', 'get').mockReturnValue('running');
+    task.emit('status.running', { status: 'running', previous: 'ready' });
+
+    // eslint-disable-next-line quotes
+    expect(lastFrame()).toMatchInlineSnapshot(`"⠋ cmd"`);
+  });
+
+  it('should print task name with success symbol', () => {
+    const { lastFrame } = render(<TaskSpinner task={task} />);
+
+    jest.spyOn(task, 'status', 'get').mockReturnValue('done');
+    task.emit('status.done', { status: 'done', previous: 'running' });
+    task.emit('completed', { status: 'done', duration: 100 });
+
+    // eslint-disable-next-line quotes
+    expect(lastFrame()).toMatchInlineSnapshot(`"√ cmd took 100ms"`);
+  });
+
+  it('should print task name with error symbol', () => {
+    const { lastFrame } = render(<TaskSpinner task={task} />);
+
+    jest.spyOn(task, 'status', 'get').mockReturnValue('failed');
+    task.emit('status.failed', { status: 'failed', previous: 'running' });
+    task.emit('completed', { status: 'failed', duration: 100 });
+
+    // eslint-disable-next-line quotes
+    expect(lastFrame()).toMatchInlineSnapshot(`"× cmd took 100ms"`);
+  });
+});
