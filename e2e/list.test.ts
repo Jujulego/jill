b51a13362189fe719f1b7b2ad7ab372a7cabc43a
Julien Capellari <julien.capellari@gmail.com>
2022-12-04 15:10:06 +0100

    feat(e2e): test list command

---
+++ e2e/list.test.ts
@@ -0,0 +1,29 @@
+import { jill } from './utils';
+import { TestBed } from '../tools/test-bed';
+
+// Setup
+let prjDir: string;
+
+beforeEach(async () => {
+  const bed = new TestBed();
+
+  bed.addWorkspace('wks-a');
+  bed.addWorkspace('wks-b');
+  bed.addWorkspace('wks-c');
+
+  prjDir = await bed.createProjectDirectory();
+});
+
+// Tests
+describe('jill list', () => {
+  it('should print list all workspaces', async () => {
+    const res = await jill(['list'], { cwd: prjDir });
+
+    await expect(res.screen.screen).toEqualLines([
+      'main',
+      'wks-a',
+      'wks-b',
+      'wks-c'
+    ]);
+  });
+});
