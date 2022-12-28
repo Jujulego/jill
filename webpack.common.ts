df8bbe3166a0d6e8ca0c5b8815bd1540f9fc9a46
Julien Capellari <julien.capellari@gmail.com>
2022-12-28 11:57:15 +0100

    chore: bundle yargs

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,38 +1,15 @@
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import { Configuration, Entry } from 'webpack';
-import nodeExternals from 'webpack-node-externals';
-import fs from 'node:fs/promises';
+import { Configuration } from 'webpack';
 import path from 'node:path';
 
 // Config
 const commonConfig: Configuration = {
   devtool: 'source-map',
   target: 'node',
-  entry: async () => {
-    const entry: Entry = {
-      main: './src/main',
-    };
-
-    const commands = await fs.readdir('./src/commands', { withFileTypes: true });
-
-    for (const command of commands) {
-      if (!command.isFile()) continue;
-
-      const filename = path.parse(command.name);
-
-      entry[`commands-${filename.name}`] = {
-        import: `./src/commands/${filename.base}`,
-        filename: `commands/${filename.name}.js`,
-        library: { type: 'commonjs2' },
-      };
-    }
-
-    return entry;
-  },
+  entry: './src/main',
   output: {
     clean: true,
-    path: path.resolve(__dirname, 'dist'),
-    enabledLibraryTypes: ['commonjs2'],
+    path: path.resolve(__dirname, 'dist')
   },
   optimization: {
     runtimeChunk: 'single',
@@ -66,12 +43,6 @@ const commonConfig: Configuration = {
   },
   externals: [
     'ws', // used only by ink for devtools
-    nodeExternals({
-      modulesFromFile: {
-        includeInBundle: 'devDependencies',
-        excludeFromBundle: 'dependencies',
-      } as any
-    }),
   ],
   plugins: [
     new ForkTsCheckerWebpackPlugin(),
