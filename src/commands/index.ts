60577edd7b3da9fadd2603b0b3b63453055c99a6
Julien Capellari <julien.capellari@gmail.com>
2022-12-28 13:44:18 +0100

    chore: bundle moo

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,8 @@
 import each from './each';
+import group from './group';
 import list from './list';
 import run from './run';
 import tree from './tree';
 
 // Commands
-export const commands = [each, list, run, tree];
+export const commands = [each, group, list, run, tree];
