3805dbbabf241e17ff7b709e5286e1b2ec7f2af3
Julien Capellari <julien.capellari@gmail.com>
2022-12-31 15:20:13 +0100

    feat: test config loader

--- src/services/config/loader.ts
+++ src/services/config/loader.ts
@@ -13,42 +13,42 @@ import { Config } from './types';
 export const CONFIG: int.ServiceIdentifier<Config> = Symbol('jujulego:jill:Config');
 
 // Loader
+export async function configLoader() {
+  const logger = container.get(Logger).child({ label: 'config' });
+  const explorer = container.get(CONFIG_EXPLORER);
+  const validator = container.get(CONFIG_VALIDATOR);
+
+  // Load file
+  const loaded = await explorer.search();
+  const config = loaded?.config ?? {};
+
+  // Validate
+  if (!validator(config)) {
+    const ajv = container.get(AJV);
+    const errors = ajv.errorsText(validator.errors, { separator: '\n- ', dataVar: 'config' });
+
+    logger.error(`Errors in config file:\n- ${errors}`);
+    process.exit(1);
+
+    return {};
+  }
+
+  if (loaded) {
+    // Resolve paths relative to config file
+    const base = path.dirname(loaded.filepath);
+    config.plugins = config.plugins?.map((plugin) => path.resolve(base, plugin));
+
+    // Apply on logger
+    if (config.verbose) {
+      container.get(Logger).level = config.verbose;
+    }
+
+    logger.verbose(`Loaded ${loaded.filepath} config file`);
+  }
+
+  return config;
+}
+
 container
-  .bind(CONFIG).toDynamicValue(async (ctx) => {
-    const logger = ctx.container.get(Logger).child({ label: 'config' });
-    const explorer = ctx.container.get(CONFIG_EXPLORER);
-    const validator = ctx.container.get(CONFIG_VALIDATOR);
-
-    // Load file
-    const loaded = await explorer.search();
-    const config = loaded?.config ?? {};
-
-    // Validate
-    if (!validator(config)) {
-      const ajv = ctx.container.get(AJV);
-      const errors = ajv.errorsText(validator.errors, { separator: '\n- ', dataVar: 'config' });
-
-
-      logger.error(
-        'Errors in config file:\n' +
-        `- ${errors}`
-      );
-      process.exit(1);
-    }
-
-    if (loaded) {
-      // Resolve paths relative to config file
-      const base = path.dirname(loaded.filepath);
-      config.plugins = config.plugins?.map((plugin) => path.resolve(base, plugin));
-
-      // Apply on logger
-      if (config.verbose) {
-        ctx.container.get(Logger).level = config.verbose;
-      }
-
-      logger.verbose(`Loaded ${loaded.filepath} config file`);
-    }
-
-    return config;
-  })
+  .bind(CONFIG).toDynamicValue(configLoader)
   .inSingletonScope();
