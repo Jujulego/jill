b1ca0b8f165e1eb75fea4f646eadc5791642b9de
Julien Capellari <julien.capellari@gmail.com>
2023-02-21 23:42:41 +0100

    feat: test task command

---
+++ tests/modules/task-command.test.tsx
@@ -0,0 +1,134 @@
+import { SpawnTask, type Task, type TaskManager } from '@jujulego/tasks';
+import { cleanup, render } from 'ink-testing-library';
+import { injectable } from 'inversify';
+import symbols from 'log-symbols';
+import { type ArgumentsCamelCase } from 'yargs';
+
+import { INK_APP } from '@/src/ink.config';
+import { container } from '@/src/inversify.config';
+import { TaskCommand } from '@/src/modules/task-command';
+import { type Workspace, type WorkspaceContext } from '@/src/project/workspace';
+import Layout from '@/src/ui/layout';
+import { printJson } from '@/src/utils/json';
+
+import { TestBed } from '@/tools/test-bed';
+import { flushPromises, spyLogger, wrapInkTestApp } from '@/tools/utils';
+import { TASK_MANAGER } from '@/src/tasks/task-manager.config';
+
+// Class
+@injectable()
+class TestTaskCommand extends TaskCommand {
+  // Methods
+  // eslint-disable-next-line require-yield
+  * prepare(args: ArgumentsCamelCase<unknown>): Generator<Task> {
+    return;
+  }
+}
+
+// Setup
+let app: ReturnType<typeof render>;
+let manager: TaskManager;
+let command: TaskCommand;
+
+let bed: TestBed;
+let wks: Workspace;
+let task: SpawnTask<WorkspaceContext>;
+
+jest.mock('@/src/utils/json');
+
+beforeEach(async () => {
+  container.snapshot();
+
+  bed = new TestBed();
+  wks = bed.addWorkspace('wks');
+  task = new SpawnTask('cmd', [], { workspace: wks, script: 'cmd' }, {
+    logger: spyLogger,
+  });
+
+  app = render(<Layout/>);
+  container.rebind(INK_APP).toConstantValue(wrapInkTestApp(app));
+
+  manager = container.get(TASK_MANAGER);
+  command = container.resolve(TestTaskCommand);
+
+  // Mocks
+  jest.resetAllMocks();
+  jest.restoreAllMocks();
+
+  jest.spyOn(command, 'prepare').mockImplementation(function* () {
+    yield task;
+  });
+
+  jest.spyOn(manager, 'add').mockImplementation();
+  jest.spyOn(manager, 'tasks', 'get').mockReturnValue([task]);
+});
+
+afterEach(() => {
+  container.restore();
+  cleanup();
+});
+
+// Tests
+describe('TaskCommand', () => {
+  describe('run mode', () => {
+    it('should start yielded tasks and print their status', async () => {
+      // Run command
+      const prom = command.handler({ $0: 'jill', _: [] });
+      await flushPromises();
+
+      // should add task to manager
+      expect(manager.add).toHaveBeenCalledWith(task);
+
+      // should print task spinner
+      expect(app.lastFrame()).toEqual(expect.ignoreColor(/^. Running cmd in wks$/));
+
+      // complete task
+      jest.spyOn(task, 'status', 'get').mockReturnValue('done');
+      task.emit('status.done', { status: 'done', previous: 'running' });
+      task.emit('completed', { status: 'done', duration: 100 });
+
+      await prom;
+
+      // should print task completed
+      expect(app.lastFrame()).toEqual(expect.ignoreColor(`${symbols.success} Running cmd in wks (took 100ms)`));
+    });
+
+    it('should exit 1 if a task fails', async () => {
+      jest.spyOn(process, 'exit').mockImplementation();
+
+      // Run command
+      const prom = command.handler({ $0: 'jill', _: [] });
+      await flushPromises();
+
+      // complete task
+      jest.spyOn(task, 'status', 'get').mockReturnValue('failed');
+      task.emit('status.failed', { status: 'failed', previous: 'running' });
+      task.emit('completed', { status: 'failed', duration: 100 });
+
+      await prom;
+
+      // should print task failed
+      expect(app.lastFrame()).toEqual(expect.ignoreColor(`${symbols.error} Running cmd in wks (took 100ms)`));
+      expect(process.exit).toHaveBeenCalledWith(1);
+    });
+  });
+
+  describe('plan mode', () => {
+    it('should print plan in a list (--plan --plan-mode list)', async () => {
+      // Run command
+      await command.handler({ $0: 'jill', _: [], plan: true, planMode: 'list' });
+
+      expect(app.lastFrame()).toEqualLines([
+        expect.ignoreColor('Id      Name  Workspace  Group  Depends on'),
+        expect.ignoreColor(`${task.id.substring(0, 6)}  ${task.name}   ${wks.name}`),
+      ]);
+    });
+
+    it('should print plan in json (--plan --plan-mode json)', async () => {
+      // Run command
+      await command.handler({ $0: 'jill', _: [], plan: true, planMode: 'json' });
+
+      expect(printJson).toHaveBeenCalledWith([task.summary]);
+    });
+  });
+});
