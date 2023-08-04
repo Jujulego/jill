9ec7e2af08d5a138ee0abc27b95deac1b61f5335
Julien Capellari <julien.capellari@gmail.com>
2023-08-04 11:46:23 +0200

    fix: build error

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,8 @@ export type Awaitable<T> = T | Promise<T>;
 export type AwaitableGenerator<T> = Generator<T> | AsyncGenerator<T>;
 
 export interface Type<T = unknown> {
-  new(...args: unknown[]): T
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  new(...args: any[]): T
 }
 
 export interface Class<T = unknown> extends Type<T> {
