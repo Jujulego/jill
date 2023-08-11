bceb9ef067e505235a807d819ba507c439ace382
Julien Capellari <julien.capellari@gmail.com>
2023-08-11 12:47:57 +0200

    fix: adapt plugin e2e tests

--- e2e/plugin.test.ts
+++ e2e/plugin.test.ts
@@ -2,27 +2,28 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import url from 'node:url';
 
-import { jill, withPackageManager } from './utils';
+import { jill } from './utils';
 import { TestBed } from '@/tools/test-bed';
 
-describe('jill test (command from plugin)', () => void withPackageManager((packageManager) => {
-  // Setup
-  let prjDir: string;
+describe('jill test (command from plugin)', () => {
+  describe.each(['npm', 'yarn'] as const)('using %s', (packageManager) => {
+    // Setup
+    let prjDir: string;
 
-  beforeEach(async () => {
-    // Create project directory
-    const bed = new TestBed();
-    bed.config = {
-      plugins: ['./plugin.mjs']
-    };
+    beforeEach(async () => {
+      // Create project directory
+      const bed = new TestBed();
+      bed.config = {
+        plugins: ['./plugin.mjs']
+      };
 
-    prjDir = await bed.createProjectPackage(packageManager);
-    const jillPath = url.pathToFileURL(path.resolve(__dirname, '../dist/index.mjs'));
+      prjDir = await bed.createProjectPackage(packageManager);
+      const jillPath = url.pathToFileURL(path.resolve(__dirname, '../dist/index.mjs'));
 
-    // Add plugin code
-    await fs.writeFile(path.join(prjDir, 'plugin.mjs'),
-      // language=javascript
-      `
+      // Add plugin code
+      await fs.writeFile(path.join(prjDir, 'plugin.mjs'),
+        // language=javascript
+        `
 import { Command, Plugin } from ${JSON.stringify(jillPath)};
 
 // Command
@@ -49,18 +50,19 @@ Plugin({
 
 export default TestPlugin;
 `);
-  }, 15000);
+    }, 15000);
 
-  afterEach(async () => {
-    await fs.rm(prjDir, { recursive: true });
+    afterEach(async () => {
+      await fs.rm(prjDir, { recursive: true });
+    });
+
+    // Tests
+    it('should run command loaded from plugin file', async () => {
+      const res = await jill('test', { cwd: prjDir });
+
+      expect(res.stdout).toEqual([
+        'this is a test plugin !'
+      ]);
+    });
   });
-
-  // Tests
-  it('should run command loaded from plugin file', async () => {
-    const res = await jill('test', { cwd: prjDir });
-
-    expect(res.stdout).toEqual([
-      'this is a test plugin !'
-    ]);
-  });
-}), { timeout: 10000 });
+}, { timeout: 10000 });
