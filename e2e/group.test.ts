22c7f9689a19402cf62411aed8ba6fb0a443691a
Julien Capellari <julien.capellari@gmail.com>
2023-08-11 12:45:22 +0200

    fix: adapt group e2e tests

--- e2e/group.test.ts
+++ e2e/group.test.ts
@@ -4,242 +4,232 @@ import path from 'node:path';
 import { TestBed } from '@/tools/test-bed';
 import { fileExists } from '@/tools/utils';
 
-import { jill, withPackageManager } from './utils';
+import { jill } from './utils';
 
-describe('jill group', () => void withPackageManager((packageManager) => {
-  // Setup
-  let prjDir: string;
+describe('jill group', () => {
+  describe.each(['npm', 'yarn'] as const)('using %s', (packageManager) => {
+    // Setup
+    let prjDir: string;
 
-  beforeEach(async () => {
-    const bed = new TestBed();
+    beforeEach(async () => {
+      const bed = new TestBed();
 
-    const wksC = bed.addWorkspace('wks-c', {
-      scripts: {
-        // language=bash
-        build: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'build\')"',
-        // language=bash
-        test1: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'test1\')"',
-        // language=bash
-        test2: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'test2\')"',
-        // language=bash
-        fails: 'node -e "process.exit(1)"',
-      }
+      const wksC = bed.addWorkspace('wks-c', {
+        scripts: {
+          // language=bash
+          build: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'build\')"',
+          // language=bash
+          test1: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'test1\')"',
+          // language=bash
+          test2: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'test2\')"',
+          // language=bash
+          fails: 'node -e "process.exit(1)"',
+        }
+      });
+      const wksB = bed.addWorkspace('wks-b', {
+        scripts: {
+          // language=bash
+          test1: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'test1\')"',
+          // language=bash
+          test2: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'test2\')"',
+        }
+      })
+        .addDependency(wksC, true);
+      bed.addWorkspace('wks-a')
+        .addDependency(wksB)
+        .addDependency(wksC, true);
+
+      prjDir = await bed.createProjectPackage(packageManager);
+    }, 15000);
+
+    afterEach(async () => {
+      await fs.rm(prjDir, { recursive: true });
     });
-    const wksB = bed.addWorkspace('wks-b', {
-      scripts: {
-        // language=bash
-        test1: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'test1\')"',
-        // language=bash
-        test2: 'node -e "require(\'node:fs\').writeFileSync(\'script.txt\', \'test2\')"',
-      }
-    })
-      .addDependency(wksC, true);
-    bed.addWorkspace('wks-a')
-      .addDependency(wksB)
-      .addDependency(wksC, true);
 
-    prjDir = await bed.createProjectPackage(packageManager);
-  }, 15000);
+    // Tests
+    describe('parallel group', () => {
+      it('should run wks-c both test1 and test2 scripts in parallel', async () => {
+        const res = await jill('group -w wks-c "test1 // test2"', { cwd: prjDir });
 
-  afterEach(async () => {
-    await fs.rm(prjDir, { recursive: true });
-  });
+        // Check jill output
+        expect(res.code).toBe(0);
 
-  // Tests
-  describe('parallel group', () => {
-    it('should run wks-c both test1 and test2 scripts in parallel', async () => {
-      const res = await jill('group -w wks-c "test1 // test2"', { cwd: prjDir });
+        expect(res.screen.screen).toMatchLines([
+          expect.ignoreColor(/^. In parallel \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test1 in wks-c \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test2 in wks-c \(took [0-9.]+m?s\)$/),
+        ]);
+
+        // Check script result
+        await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
+          .resolves.toMatch(/test[12]/);
+      });
+
+      it('should run wks-c both test1 and fails scripts in parallel and exit 1', async () => {
+        const res = await jill('group -w wks-c "test1 // fails"', { cwd: prjDir });
+
+        // Check jill output
+        expect(res.code).toBe(1);
+
+        expect(res.screen.screen).toMatchLines([
+          expect.ignoreColor(/^. In parallel \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test1 in wks-c \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running fails in wks-c \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {4}.( yarn exec)? node -e "process.exit\(1\)" \(took [0-9.]+m?s\)$/),
+        ]);
+
+        // Check script result
+        await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
+          .resolves.toBe('test1');
+      });
+
+      it('should run wks-c build then run wks-b both test1 and test2 scripts in parallel', async () => {
+        const res = await jill('group -w wks-b "test1 // test2"', { cwd: prjDir });
+
+        // Check jill output
+        expect(res.code).toBe(0);
+
+        expect(res.screen.screen).toMatchLines([
+          expect.ignoreColor(/^. Running build in wks-c \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^. In parallel \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test1 in wks-b \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test2 in wks-b \(took [0-9.]+m?s\)$/),
+        ]);
+
+        // Check script result
+        await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
+          .resolves.toBe('build');
+
+        await expect(fs.readFile(path.join(prjDir, 'wks-b', 'script.txt'), 'utf8'))
+          .resolves.toMatch(/test[12]/);
+      });
+    });
+
+    describe('sequence group', () => {
+      it('should run wks-c both test1 and test2 scripts in sequence', async () => {
+        const res = await jill('group -w wks-c "test1 -> test2"', { cwd: prjDir });
+
+        // Check jill output
+        expect(res.code).toBe(0);
+
+        expect(res.screen.screen).toMatchLines([
+          expect.ignoreColor(/^. In sequence \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test1 in wks-c \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test2 in wks-c \(took [0-9.]+m?s\)$/),
+        ]);
+
+        // Check script result
+        await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
+          .resolves.toBe('test2');
+      });
+
+      it('should run wks-c both test1 and fails scripts in sequence and exit 1', async () => {
+        const res = await jill('group -w wks-c "test1 -> fails"', { cwd: prjDir });
+
+        // Check jill output
+        expect(res.code).toBe(1);
+
+        expect(res.screen.screen).toMatchLines([
+          expect.ignoreColor(/^. In sequence \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test1 in wks-c \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running fails in wks-c \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {4}.( yarn exec)? node -e "process.exit\(1\)" \(took [0-9.]+m?s\)$/),
+        ]);
+
+        // Check script result
+        await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
+          .resolves.toBe('test1');
+      });
+
+      it('should run wks-c build then run wks-b both test1 and test2 scripts in sequence', async () => {
+        const res = await jill('group -w wks-b "test1 -> test2"', { cwd: prjDir });
+
+        // Check jill output
+        expect(res.code).toBe(0);
+
+        expect(res.screen.screen).toMatchLines([
+          expect.ignoreColor(/^. Running build in wks-c \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^. In sequence \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test1 in wks-b \(took [0-9.]+m?s\)$/),
+          expect.ignoreColor(/^ {2}. Running test2 in wks-b \(took [0-9.]+m?s\)$/),
+        ]);
+
+        // Check script result
+        await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
+          .resolves.toBe('build');
+
+        await expect(fs.readFile(path.join(prjDir, 'wks-b', 'script.txt'), 'utf8'))
+          .resolves.toBe('test2');
+      });
+    });
+
+    it('should print task plan and do not run any script', async () => {
+      const res = await jill('group -w wks-c --plan --plan-mode json "test1 -> test2"', { cwd: prjDir });
 
       // Check jill output
       expect(res.code).toBe(0);
+      expect(res.stdout).toHaveLength(1);
 
-      expect(res.screen.screen).toMatchLines([
-        expect.ignoreColor(/^. In parallel \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test1 in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test2 in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-      ]);
+      const plan = JSON.parse(res.stdout[0]);
+      expect(plan).toHaveLength(5);
 
-      // Check script result
-      await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
-        .resolves.toMatch(/test[12]/);
-    });
+      expect(plan[0]).toMatchObject({
+        id: expect.stringMatching(/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/),
+        isGroup: true,
+        context: {}
+      });
 
-    it('should run wks-c both test1 and fails scripts in parallel and exit 1', async () => {
-      const res = await jill('group -w wks-c "test1 // fails"', { cwd: prjDir });
+      expect(plan[1]).toMatchObject({
+        id: expect.stringMatching(/[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}/),
+        isGroup: true,
+        groupId: plan[0].id,
+        context: {
+          script: 'test1',
+          workspace: {
+            name: 'wks-c',
+            cwd: path.join(prjDir, 'wks-c')
+          }
+        }
+      });
 
-      // Check jill output
-      expect(res.code).toBe(1);
+      expect(plan[2]).toMatchObject({
+        id: expect.stringMatching(/[0-9a-f]{32}/),
+        groupId: plan[1].id,
+        context: {
+          command: 'node',
+          workspace: {
+            name: 'wks-c',
+            cwd: path.join(prjDir, 'wks-c')
+          }
+        }
+      });
 
-      expect(res.screen.screen).toMatchLines([
-        expect.ignoreColor(/^. In parallel \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test1 in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running fails in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "process.exit\(1\)" \(took [0-9.]+m?s\)$/),
-      ]);
+      expect(plan[3]).toMatchObject({
+        id: expect.stringMatching(/[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}/),
+        isGroup: true,
+        groupId: plan[0].id,
+        context: {
+          script: 'test2',
+          workspace: {
+            name: 'wks-c',
+            cwd: path.join(prjDir, 'wks-c')
+          }
+        }
+      });
 
-      // Check script result
-      await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
-        .resolves.toBe('test1');
-    });
+      expect(plan[4]).toMatchObject({
+        id: expect.stringMatching(/[0-9a-f]{32}/),
+        groupId: plan[3].id,
+        context: {
+          command: 'node',
+          workspace: {
+            name: 'wks-c',
+            cwd: path.join(prjDir, 'wks-c')
+          }
+        }
+      });
 
-    it('should run wks-c build then run wks-b both test1 and test2 scripts in parallel', async () => {
-      const res = await jill('group -w wks-b "test1 // test2"', { cwd: prjDir });
-
-      // Check jill output
-      expect(res.code).toBe(0);
-
-      expect(res.screen.screen).toMatchLines([
-        expect.ignoreColor(/^. Running build in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^. In parallel \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test1 in wks-b \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test2 in wks-b \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-      ]);
-
-      // Check script result
-      await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
-        .resolves.toBe('build');
-
-      await expect(fs.readFile(path.join(prjDir, 'wks-b', 'script.txt'), 'utf8'))
-        .resolves.toMatch(/test[12]/);
+      await expect(fileExists(path.join(prjDir, 'wks-c', 'script.txt'))).resolves.toBe(false);
     });
   });
-
-  describe('sequence group', () => {
-    it('should run wks-c both test1 and test2 scripts in sequence', async () => {
-      const res = await jill('group -w wks-c "test1 -> test2"', { cwd: prjDir });
-
-      // Check jill output
-      expect(res.code).toBe(0);
-
-      expect(res.screen.screen).toMatchLines([
-        expect.ignoreColor(/^. In sequence \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test1 in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test2 in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-      ]);
-
-      // Check script result
-      await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
-        .resolves.toBe('test2');
-    });
-
-    it('should run wks-c both test1 and fails scripts in sequence and exit 1', async () => {
-      const res = await jill('group -w wks-c "test1 -> fails"', { cwd: prjDir });
-
-      // Check jill output
-      expect(res.code).toBe(1);
-
-      expect(res.screen.screen).toMatchLines([
-        expect.ignoreColor(/^. In sequence \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test1 in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running fails in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "process.exit\(1\)" \(took [0-9.]+m?s\)$/),
-      ]);
-
-      // Check script result
-      await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
-        .resolves.toBe('test1');
-    });
-
-    it('should run wks-c build then run wks-b both test1 and test2 scripts in sequence', async () => {
-      const res = await jill('group -w wks-b "test1 -> test2"', { cwd: prjDir });
-
-      // Check jill output
-      expect(res.code).toBe(0);
-
-      expect(res.screen.screen).toMatchLines([
-        expect.ignoreColor(/^. Running build in wks-c \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^. In sequence \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test1 in wks-b \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {2}. Running test2 in wks-b \(took [0-9.]+m?s\)$/),
-        expect.ignoreColor(/^ {4}.( yarn exec)? node -e "require\('node:fs'\).+ \(took [0-9.]+m?s\)$/),
-      ]);
-
-      // Check script result
-      await expect(fs.readFile(path.join(prjDir, 'wks-c', 'script.txt'), 'utf8'))
-        .resolves.toBe('build');
-
-      await expect(fs.readFile(path.join(prjDir, 'wks-b', 'script.txt'), 'utf8'))
-        .resolves.toBe('test2');
-    });
-  });
-
-  it('should print task plan and do not run any script', async () => {
-    const res = await jill('group -w wks-c --plan --plan-mode json "test1 -> test2"', { cwd: prjDir });
-
-    // Check jill output
-    expect(res.code).toBe(0);
-    expect(res.stdout).toHaveLength(1);
-
-    const plan = JSON.parse(res.stdout[0]);
-    expect(plan).toHaveLength(5);
-
-    expect(plan[0]).toMatchObject({
-      id: expect.stringMatching(/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/),
-      isGroup: true,
-      context: {}
-    });
-
-    expect(plan[1]).toMatchObject({
-      id: expect.stringMatching(/[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}/),
-      isGroup: true,
-      groupId: plan[0].id,
-      context: {
-        script: 'test1',
-        workspace: {
-          name: 'wks-c',
-          cwd: path.join(prjDir, 'wks-c')
-        }
-      }
-    });
-
-    expect(plan[2]).toMatchObject({
-      id: expect.stringMatching(/[0-9a-f]{32}/),
-      groupId: plan[1].id,
-      context: {
-        command: 'node',
-        workspace: {
-          name: 'wks-c',
-          cwd: path.join(prjDir, 'wks-c')
-        }
-      }
-    });
-
-    expect(plan[3]).toMatchObject({
-      id: expect.stringMatching(/[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}/),
-      isGroup: true,
-      groupId: plan[0].id,
-      context: {
-        script: 'test2',
-        workspace: {
-          name: 'wks-c',
-          cwd: path.join(prjDir, 'wks-c')
-        }
-      }
-    });
-
-    expect(plan[4]).toMatchObject({
-      id: expect.stringMatching(/[0-9a-f]{32}/),
-      groupId: plan[3].id,
-      context: {
-        command: 'node',
-        workspace: {
-          name: 'wks-c',
-          cwd: path.join(prjDir, 'wks-c')
-        }
-      }
-    });
-
-    await expect(fileExists(path.join(prjDir, 'wks-c', 'script.txt'))).resolves.toBe(false);
-  });
-}), { timeout: 10000 });
+}, { timeout: 10000 });
