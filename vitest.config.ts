792f1cf88131166d05db3766de752da5a7d81fbf
Julien Capellari <julien.capellari@neoxia.com>
2023-09-07 18:34:26 +0200

    chore: fix tests errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,3 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { swc } from 'rollup-plugin-swc3';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
@@ -31,7 +29,6 @@ export default defineConfig({
             runtime: 'automatic'
           }
         },
-        baseUrl: dirname(fileURLToPath(import.meta.url)),
         paths: {
           '@/src/*': ['./src/*'],
           '@/tools/*': ['./tools/*']
