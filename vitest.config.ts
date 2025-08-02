dbba3bb26f46937c3a43d8d34c934612a1b2703e
Julien Capellari <julien.capellari@gmail.com>
2025-08-02 16:22:52 +0200

    chore: rework ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,11 +5,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    reporters: ['default', 'junit'],
     setupFiles: ['tools/setup.ts'],
     coverage: {
       include: ['src/**', 'tools/**'],
       reporter: ['text', 'lcovonly'],
-    }
+    },
+    outputFile: {
+      junit: 'junit-report.xml'
+    },
   },
   plugins: [
     tsconfigPaths(),
