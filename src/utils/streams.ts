88118df582961d067f6531272f2b659e3f2465fc
Julien Capellari <julien.capellari@gmail.com>
2025-08-11 01:00:27 +0200

    test: streamLines$

--- src/utils/streams.ts
+++ src/utils/streams.ts
@@ -1,41 +1,11 @@
 import { type SpawnTask } from '@jujulego/tasks';
-import { iterate$, type Observable, observable$, off$, once$ } from 'kyrielle';
+import { type Observable, observable$, off$, once$ } from 'kyrielle';
 
 // Utils
 export async function* combine<T>(...generators: AsyncGenerator<T>[]): AsyncGenerator<T> {
   for (const gen of generators) {
     yield* gen;
   }
-}
-
-/** @deprecated */
-export async function *streamLines(task: SpawnTask): AsyncGenerator<string> {
-  // Abort
-  let completed = false;
-
-  once$(task.events$, 'completed', () => {
-    completed = true;
-  });
-
-  // Stream
-  let current = '';
-
-  for await (const chunk of iterate$(task.events$.getOrigin('stream').getOrigin('stdout'))) {
-    const data = current + chunk.data.toString('utf-8');
-    const lines = data.split(/\r?\n/);
-
-    current = lines.pop() ?? '';
-
-    for (const line of lines) {
-      yield line;
-    }
-
-    if (completed) {
-      break;
-    }
-  }
-
-  yield current;
 }
 
 export function streamLines$(task: SpawnTask): Observable<string> {
