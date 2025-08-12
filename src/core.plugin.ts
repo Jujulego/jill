96e155ff0d3eea6acf8735afa69f49c63668be16
Julien Capellari <julien.capellari@gmail.com>
2025-08-12 14:11:29 +0200

    chore: remove legacy list command

--- src/core.plugin.ts
+++ src/core.plugin.ts
@@ -1,7 +1,6 @@
 import { EachCommand } from '@/src/commands/each.js';
 import { ExecCommand } from '@/src/commands/exec.js';
 import { GroupCommand } from '@/src/commands/group.js';
-import { ListCommand } from '@/src/commands/list.jsx';
 import { RunCommand } from '@/src/commands/run.js';
 import { Plugin } from '@/src/modules/plugin.js';
 
@@ -12,7 +11,6 @@ import { Plugin } from '@/src/modules/plugin.js';
     EachCommand,
     ExecCommand,
     GroupCommand,
-    ListCommand,
     RunCommand,
   ]
 })
