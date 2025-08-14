753011a8c01bf4426cbd954b820cb1a88a9e1e5d
Julien Capellari <julien.capellari@gmail.com>
2025-08-14 10:46:54 +0200

    feat: complete workspace impl

--- src/projects/workspace.ts
+++ src/projects/workspace.ts
@@ -1,9 +1,14 @@
+import type { Task, TaskOptions } from '@jujulego/tasks';
 import { inject$ } from '@kyrielle/injector';
 import { type Logger, withLabel } from '@kyrielle/logger';
+import { waitFor$ } from 'kyrielle';
 import path from 'node:path';
 import type { Package } from 'normalize-package-data';
 import { satisfies } from 'semver';
 import { GitService } from '../commons/git.service.js';
+import { ConfigService } from '../config/config.service.js';
+import { CommandTask } from '../tasks/command-task.js';
+import { ScriptTask } from '../tasks/script-task.js';
 import { LOGGER } from '../tokens.js';
 import { combine } from '../utils/streams.js';
 import type { Project } from './project.js';
@@ -14,6 +19,7 @@ export class Workspace {
   private readonly _logger: Logger;
   private readonly _git = inject$(GitService);
   private readonly _root: string;
+  private readonly _tasks = new Map<string, ScriptTask>();
 
   // Constructor
   constructor(
@@ -26,6 +32,28 @@ export class Workspace {
   }
 
   // Methods
+  private async _buildDependencies(task: Task, opts: WorkspaceRunOptions) {
+    const generators: AsyncGenerator<Workspace, void>[] = [];
+
+    switch (opts.buildDeps ?? 'all') {
+      case 'all':
+        generators.unshift(this.devDependencies());
+
+      // eslint-disable-next no-fallthrough
+      case 'prod':
+        generators.unshift(this.dependencies());
+    }
+
+    // Build deps
+    for await (const dep of combine(...generators)) {
+      const build = await dep.build(opts);
+
+      if (build) {
+        task.dependsOn(build);
+      }
+    }
+  }
+
   private async* _loadDependencies(dependencies: Record<string, string>, kind: string): AsyncGenerator<Workspace, void> {
     for (const [dep, range] of Object.entries(dependencies)) {
       const ws = await this.project.workspace(dep);
@@ -100,9 +128,60 @@ export class Workspace {
     }
   }
 
+  async build(opts: WorkspaceRunOptions = {}): Promise<ScriptTask | null> {
+    const script = opts.buildScript ?? 'build';
+    const task = await this.run(script, [], opts);
+
+    if (!task) {
+      this._logger.warning(`will not be built (no "${script}" script found)`);
+    }
+
+    return task;
+  }
+
+  async exec(command: string, args: string[] = [], opts: WorkspaceRunOptions = {}): Promise<CommandTask> {
+    const pm = await this.project.packageManager();
+    const task = new CommandTask(this, command, args, {
+      ...opts,
+      logger: this._logger.child(withLabel(`${this.name}$${command}`)),
+      superCommand: pm === 'yarn' ? ['yarn', 'exec'] : undefined
+    });
+
+    await this._buildDependencies(task, opts);
+
+    return task;
+  }
+
   getScript(script: string): string | null {
     const { scripts = {} } = this.manifest;
     return scripts[script] || null;
+  }
+
+  async run(script: string, args: string[] = [], opts: WorkspaceRunOptions = {}): Promise<ScriptTask | null> {
+    // Script not found
+    if (!this.getScript(script)) {
+      return null;
+    }
+
+    // Create task if it doesn't exist yet
+    let task = this._tasks.get(script);
+
+    if (!task) {
+      const config = await waitFor$(inject$(ConfigService).config$);
+
+      task = new ScriptTask(this, script, args, {
+        ...opts,
+        logger: this._logger.child(withLabel(`${this.name}#${script}`)),
+        runHooks: config.hooks,
+      });
+
+      await task.prepare();
+      await this._buildDependencies(task, opts);
+
+      this._tasks.set(script, task);
+    }
+
+    return task;
   }
 
   // Properties
@@ -121,4 +200,12 @@ export class Workspace {
   get version(): string {
     return this.manifest.version;
   }
+}
+
+// Types
+export type WorkspaceDepsMode = 'all' | 'prod' | 'none';
+
+export interface WorkspaceRunOptions extends Omit<TaskOptions, 'logger'> {
+  readonly buildDeps?: WorkspaceDepsMode;
+  readonly buildScript?: string;
 }
