f7c390c50a1b9730f89e3328e5e28758dd546ffe
Julien Capellari <julien.capellari@gmail.com>
2025-08-18 14:39:18 +0200

    chore: remove legacy logger

--- src/commons/logger/types.ts
+++
@@ -1,4 +0,0 @@
-import { Log as BaseLog, LogLabel, WithTimestamp } from '@jujulego/logger';
-
-// Types
-export type JillLog = WithTimestamp<BaseLog> & Partial<LogLabel>;
--- src/inversify.config.ts
+++
@@ -1,6 +0,0 @@
-import { Container } from 'inversify';
-import 'reflect-metadata/lite';
-
-// Container
-/** @deprecated */
-export const container = new Container();
