c1f60aad9d8fb63013cc903a185bbb1669d6d1bb
Julien Capellari <julien.capellari@gmail.com>
2025-08-20 18:38:22 +0200

    feat: remove explicit default integration

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,9 +1,8 @@
-import { childProcessIntegration, fsIntegration, init } from '@sentry/node';
+import { fsIntegration, init } from '@sentry/node';
 
 init({
   dsn: 'https://53e6d10c16975ebd025175d9836d039b@o4508229080055808.ingest.de.sentry.io/4509876546895952',
   integrations: [
-    childProcessIntegration(),
     fsIntegration(),
   ],
   sendDefaultPii: true,
