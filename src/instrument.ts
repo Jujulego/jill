4ce5f0a771d15666ec2625d5de1b7cc3080bfb1a
Julien Capellari <julien.capellari@gmail.com>
2025-08-20 18:32:46 +0200

    fix: disable sentry debug mode

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,7 +1,6 @@
 import { childProcessIntegration, fsIntegration, init } from '@sentry/node';
 
 init({
-  debug: true,
   dsn: 'https://53e6d10c16975ebd025175d9836d039b@o4508229080055808.ingest.de.sentry.io/4509876546895952',
   integrations: [
     childProcessIntegration(),
