fc73c18ec3ab4b3390d69548659f3fe36a2c0f5a
Julien Capellari <julien.capellari@gmail.com>
2025-08-21 16:48:11 +0200

    feat: do not report ClientErrors

--- src/cli/utils/errors.ts
+++ src/cli/utils/errors.ts
@@ -1,10 +1,7 @@
-import { captureException } from '@sentry/node';
-
 export class ClientError extends Error {
   name = 'ClientError';
 
   constructor(message: string) {
     super(message);
-    queueMicrotask(() => captureException(this, { level: 'warning' }));
   }
 }
