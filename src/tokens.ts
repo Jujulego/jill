79c3a6b2af72ea983564f6caddbf7b342edd3e37
Julien Capellari <julien.capellari@gmail.com>
2025-11-02 14:46:33 +0100

    chore: remove legacy task manager token

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,7 +1,7 @@
-import { isWorkloadEnded, scheduler$, TaskManager, WorkloadState } from '@jujulego/tasks';
+import { isWorkloadEnded, scheduler$, WorkloadState } from '@jujulego/tasks';
 import { asyncScope$, inject$, token$ } from '@kyrielle/injector';
 import { logger$, withTimestamp } from '@kyrielle/logger';
-import { getActiveSpan, getRootSpan, type Span, startInactiveSpan } from '@sentry/node';
+import { type Span, startInactiveSpan } from '@sentry/node';
 import { type Unsubscribable, waitFor$ } from 'kyrielle';
 import fs from 'node:fs';
 import process from 'node:process';
@@ -16,46 +16,6 @@ export const CONFIG = token$('Config', async () => {
 export const CWD = token$('cwd', () => process.cwd());
 export const LOGGER = token$('Logger', () => logger$(withTimestamp()));
 export const PATH_SCURRY = token$('PathScurry', () => new PathScurry('/', { fs }));
-
-/** @deprecated */
-export const TASK_MANAGER = token$('TaskManager', async () => {
-  const manager = new TaskManager({
-    jobs: (await inject$(CONFIG, asyncScope$())).jobs,
-    logger: inject$(LOGGER),
-  });
-
-  let rootSpan = getActiveSpan();
-  if (rootSpan) rootSpan = getRootSpan(rootSpan);
-
-  const spans = new Map<string, Span>();
-
-  manager.events$.on('added', (task) => {
-    // Main span
-    const span = startInactiveSpan({
-      name: task.name,
-      parentSpan: (task.group && spans.get(task.group.id)) ?? rootSpan,
-      op: 'task',
-    });
-    task.events$.on('completed', ({ status }) => {
-      span.setStatus({ code: status === 'done' ? 1 : 2 });
-      span.end();
-    });
-    spans.set(task.id, span);
-
-    // Status spans
-    let statusSpan = startInactiveSpan({ name: task.status, parentSpan: span, op: 'task.status' });
-
-    task.events$.on('status', ({ status }) => {
-      statusSpan.end();
-
-      if (!task.completed) {
-        statusSpan = startInactiveSpan({ name: status, parentSpan: span, op: 'task.status' });
-      }
-    });
-  });
-
-  return manager;
-});
 
 export const SCHEDULER = token$('Scheduler', async () => {
   const config = await inject$(CONFIG, asyncScope$());
