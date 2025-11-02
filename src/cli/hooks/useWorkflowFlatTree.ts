1ff98e2da6fb58686542dbd6135c088a2bfb3a32
Julien Capellari <julien.capellari@gmail.com>
2025-11-02 21:55:06 +0100

    feat: filter workload tree

--- src/cli/hooks/useWorkflowFlatTree.ts
+++ src/cli/hooks/useWorkflowFlatTree.ts
@@ -1,35 +1,80 @@
-import type { Workflow$, Workload$ } from '@jujulego/tasks';
-import { useMemo } from 'react';
+import { type Workflow$, type Workload$, WorkloadState } from '@jujulego/tasks';
+import { collect$, map$, off$, pipe$ } from 'kyrielle';
+import { createHash } from 'node:crypto';
+import { useEffect, useState } from 'react';
 
 // Hook
-export function useWorkflowFlatTree(workload: Workload$, verbose = false): FlatTreeWorkload[] {
-  return useMemo(() => {
-    const stack: FlatTreeWorkload[] = [{ workload, level: 0 }];
-    const tree: FlatTreeWorkload[] = [];
+export function useWorkflowFlatTree(workload: Workload$, verbose?: boolean): FlatTreeWorkload[] {
+  const [tree, setTree] = useState<FlatTreeWorkload[]>(() => buildTree(workload, verbose));
 
-    while (stack.length > 0) {
-      const item = stack.pop()!;
-      let level = item.level;
+  useEffect(() => {
+    const oldHash = hashTree(tree);
+    let dirty = false;
 
-      if (item.workload.label !== '[hidden]') {
-        tree.push(item);
-        level++;
+    function update() {
+      if (!dirty) return;
+
+      const updated = buildTree(workload, verbose);
+      if (hashTree(updated) !== oldHash) {
+        setTree(updated);
       }
 
-      if (isWorkflow(item.workload)) {
-        for (const workload of item.workload.workloads()) {
-          stack.push({ workload, level });
-        }
-      }
+      dirty = false;
     }
 
-    return tree;
-  }, [workload]);
+    const off = pipe$(tree,
+      map$(({ workload }) => workload.state$.subscribe(() => {
+        dirty = true;
+        queueMicrotask(update);
+      })),
+      collect$(off$())
+    );
+
+    return () => {
+      dirty = false;
+      off.unsubscribe();
+    };
+  }, [tree, verbose, workload]);
+  
+  return tree;
 }
 
 // Utils
+function buildTree(workload: Workload$, verbose = false) {
+  const stack: FlatTreeWorkload[] = [{ workload, level: 0 }];
+  const tree: FlatTreeWorkload[] = [];
+
+  while (stack.length > 0) {
+    const item = stack.pop()!;
+    let level = item.level;
+
+    if (item.workload.label !== '[hidden]' && (verbose || item.level === 0 || isWorkflow(item.workload) || [WorkloadState.Starting, WorkloadState.Running, WorkloadState.Failed].includes(item.workload.state()))) {
+      tree.push(item);
+      level++;
+    }
+
+    if (isWorkflow(item.workload)) {
+      for (const workload of item.workload.workloads()) {
+        stack.push({ workload, level });
+      }
+    }
+  }
+
+  return tree;
+}
+
 function isWorkflow(workload: Workload$): workload is Workflow$ {
   return 'workloads' in workload && typeof workload.workloads === 'function';
+}
+
+function hashTree(tree: FlatTreeWorkload[]) {
+  const hash = createHash('sha256');
+
+  for (const { workload } of tree) {
+    hash.push(workload.id);
+  }
+
+  return hash.digest('hex');
 }
 
 // Types
