0f6e202be3e43adcb047525f5269d21efb0d407c
Julien Capellari <julien.capellari@gmail.com>
2025-11-10 11:10:21 +0100

    feat: use snapshots in e2e tests

--- e2e/exec.test.ts
+++ e2e/exec.test.ts
@@ -113,12 +113,7 @@ describe('jill exec', () => {
       // Check jill plan
       expect(res.code).toBe(0);
 
-      expect(res.screen.screen).toMatchLines([
-        expect.ignoreColor(/^ {7}Job( {74}| {64})Depends on$/),
-        expect.ignoreColor(/^#1 {5}build *$/),
-        expect.ignoreColor(/^└─ #2 {2}(yarn exec )?node -e "require\('node:fs'\)\.writeFileSync\('script\.txt', 'build'\)" *$/),
-        expect.ignoreColor(/^#3 {5}(yarn exec )?node -e "require\('node:fs'\)\.writeFileSync\('script.txt', 'node'\)" {3}#1$/),
-      ]);
+      expect(res.screen.screen).toMatchSnapshot();
 
       await expect(fileExists(path.join(prjDir, 'wks-c', 'script.txt'))).resolves.toBe(false);
       await expect(fileExists(path.join(prjDir, 'wks-b', 'script.txt'))).resolves.toBe(false);
