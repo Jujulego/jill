ed091556dc5d2fb90e5db70c1d46b276d3bd646d
Julien Capellari <julien.capellari@gmail.com>
2025-11-24 20:52:36 +0100

    Unbundle react (#1457)
    * Unbundle react
    
    * chore: lockfile

--- src/utils/symbols.ts
+++ src/utils/symbols.ts
@@ -2,5 +2,5 @@ import isUnicodeSupported from 'is-unicode-supported';
 
 const isSupported = !isUnicodeSupported();
 
-export const success = isSupported ? '✔' : '√';
+export const success = isSupported ? '✔' : '✓';
 export const error = isSupported ? '✖' : '×';
