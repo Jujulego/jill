9c799e38bd12b21aac2592c6f9e64abac0688eec
Julien Capellari <julien.capellari@gmail.com>
2025-12-30 10:43:54 +0100

    fix: change test pool type

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       reporter: ['text', 'lcovonly'],
     },
     globals: true,
-    pool: 'forks',
+    pool: 'threads',
     reporters: ['default', 'junit'],
     setupFiles: ['tools/setup.ts'],
     outputFile: {
